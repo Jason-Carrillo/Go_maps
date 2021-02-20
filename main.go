f2d12b039d1de523aa198cfa26972769a1a6db99
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-20 00:09:43 -0700

    added different types of callers sb eb

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+type englishBot struct{}
+type spanishBot struct{}
+
 func main() {
 
 	//Different ways to create a MAP
@@ -13,17 +16,42 @@ func main() {
 	// colors := make(map[string]string)
 
 	// #3 Create a map
-	colors := map[string]string{
-		"red":   "ff0000",
-		"green": "75b271",
-		"white": "ffffff",
-	}
+	// 	colors := map[string]string{
+	// 		"red":   "ff0000",
+	// 		"green": "75b271",
+	// 		"white": "ffffff",
+	// 	}
 
-	printMap(colors)
+	// 	printMap(colors)
+	// }
+
+	// func printMap(c map[string]string) {
+	// 	for color, hex := range c {
+	// 		fmt.Println("Hex code for", color, "is", hex)
+	// 	}
+
+	eb := englishBot{}
+	// sb := spanishBot{}
+
+	printGreeting(eb)
+	// printGreeting(sb)
+
 }
 
-func printMap(c map[string]string) {
-	for color, hex := range c {
-		fmt.Println("Hex code for ", color, "is", hex)
-	}
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+// func printGreeting(sb spanishBot) {
+// 	fmt.Println(sb.getGreeting())
+// }
+
+func (eb englishBot) getGreeting() string {
+	//VERY custom logic for generating an english greeting
+	return ("Hi, There!")
+}
+
+// If you are not going to use the Placeholder  you can remove it OPTIONAL
+func (spanishBot) getGreeting() string {
+	return ("Hola!")
 }
