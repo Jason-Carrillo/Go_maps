e265e13d4da1cfe173d50f252804504998524155
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-19 23:32:10 -0700

    added delete func to a map

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var colors map[string]string
 
+	//Different ways to create a MAP
+
+	// #1 create a map
+	// var colors map[string]string
+
+	//#2 Create a map
+	colors := make(map[string]string)
+
+	//#3 Create a map
 	// colors := map[string]string{
 	// 	"red":   "ff0000",
 	// 	"green": "75b271",
 	// }
+
+	colors["white"] = "ffffff"
+
+	delete(colors, "white")
 
 	fmt.Println(colors)
 }
